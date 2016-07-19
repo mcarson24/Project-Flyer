68d390278f268b4e474d147b8bbfbcd2a428e1a4
Matthew Carson <matthew24carson@gmail.com>
2016-07-19 14:58:05 -0400

    Update gulpfile/ layout file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Project Flyer</title>
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+  </head>
+  <body>
+    <div class="container">
+        @yield('content')
+    </div>
+  </body>
+</html>
